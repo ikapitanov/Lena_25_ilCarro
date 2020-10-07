3a42d5acfc8c300a520a79209b8c9a7992a25fe7
Lena <etarnovskaya@gmail.com>
2020-10-07 18:56:47 +0300

    createde addCarTests

---
+++ src/test/java/com/ilcarro/qa/AddCarTests.java
@@ -0,0 +1,35 @@
+package com.ilcarro.qa;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class AddCarTests extends TestBase{
+    @Test
+    public void testFormLetTheCarWork(){
+        // click(By.cssSelector(".let-carwork-style_let_car__location__30BIh"));
+
+        type(By.name("[name='country']"), "Israel");
+        type(By.cssSelector(".address"), "Balfour Street");
+        type(By.cssSelector(".distance_included"), "345");
+        type(By.cssSelector(".serial_number"), "123456");
+        type(By.cssSelector(".brand"), "Volkswagen ");
+        type(By.cssSelector(".model"), "Bora");
+        type(By.cssSelector(".year"), "2002");
+        type(By.cssSelector(".engine"), "1,6");
+        type(By.cssSelector(".fuel_consumption"), "8");
+        type(By.cssSelector(".fuel"), "petrol");
+        type(By.cssSelector(".transmition"), "automatic");
+        type(By.cssSelector(".wd"), "wedfrt");
+        type(By.cssSelector(".horsepower"), "110");
+        type(By.cssSelector(".torque"), "3456");
+        type(By.cssSelector(".doors"),"4");
+        type(By.cssSelector(".seats"), "5");
+        type(By.cssSelector(".class"), "E");
+        type(By.name("[name='about']"), "dhfhjkglglgkmfjdk");
+        type(By.cssSelector(".type_feature"), "dfghjkl");
+        type(By.cssSelector(".price"), "87");
+
+        submitForm();
+
+    }
+}
